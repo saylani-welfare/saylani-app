80d26b472daf92b4e56af1e4a65286860f35716c
Saylani Welfare <saylaniwelfare78692@gmail.com>
2017-03-29 02:02:54 +0500

    admin & main page added

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,7 @@
 // export all the components in this file from the same folder it is in.
 export {default as App} from './App/App.jsx';
+export {default as Main} from './Main/Main.jsx';
+export {default as AppBarExampleIcon} from './AppBar/appbar';
+
+
+
